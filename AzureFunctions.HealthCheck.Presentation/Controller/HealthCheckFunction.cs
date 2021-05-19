203f461faf2bb19a1efe722fee656621eb8fd753
Jessé Toledo <h4ck3r.batera@gmail.com>
2021-05-18 23:56:56 -0300

    Microsoft.Extensions.Diagnostics.HealthChecks

---
+++ AzureFunctions.HealthCheck.Presentation/Controller/HealthCheckFunction.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Threading;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace AzureFunctions.HealthCheck.Presentation.Controller
+{
+    public class HealthCheckFunction
+    {
+        private readonly HealthCheckService _healthCheck;
+        public HealthCheckFunction(HealthCheckService healthCheck)
+        {
+            _healthCheck = healthCheck;
+        }
+
+        [FunctionName("Heartbeat")]
+        public async Task<IActionResult> Heartbeat(
+           [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "heartbeat")] HttpRequest req,
+           ILogger log, CancellationToken cancellationToken)
+        {
+            log.Log(LogLevel.Information, "Received heartbeat request");
+
+            var healthReport = await _healthCheck.CheckHealthAsync(cancellationToken);
+
+            var json = JsonSerializer.Serialize(healthReport,
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true,
+                        Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) }
+                    });
+
+            return new OkObjectResult(json);
+        }
+    }
+}
---
+++ AzureFunctions.HealthCheck.Presentation/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using AzureFunctions.HealthCheck.Presentation;
+
+[assembly: WebJobsStartup(typeof(Startup))]
+namespace AzureFunctions.HealthCheck.Presentation
+{
+    public class Startup : IWebJobsStartup
+    {
+        public void Configure(IWebJobsBuilder builder)
+        {
+            ConfigureServices(builder.Services);
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging();
+
+            services.AddHealthChecks();
+        }
+    }
+}
